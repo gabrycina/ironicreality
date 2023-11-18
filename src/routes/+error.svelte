c757791819d0561617b853ac4d62143b160bb1ba
gabrycina <c.gabriele.info@gmail.com>
2023-11-18 01:51:02 +0100

    Add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="h-screen w-full flex flex-col justify-center items-center">
+	<p class="font-bold text-9xl">404</p>
+	<a href="/" class="underline mb-52">Home</a>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="bg-gray-800 h-screen flex flex-col items-center justify-center">
+	<p class="text-white text-5xl md:text-9xl font-bold mb-52">Ironic Reality</p>
+	<a href="/blog" class="text-2xl underline text-white">Read</a>
+</div>
